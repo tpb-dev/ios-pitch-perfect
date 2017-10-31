70fe3e5a2ee2bb44b74fde2d1e0c1a70b01a72a2
Randall Tom <randalltom@sfo315.ca.revelsystems.com>
2017-10-31 08:30:30 -0700

    2

--- ios-pitch-perfect/RecordSoundsViewController.swift
+++ ios-pitch-perfect/RecordSoundsViewController.swift
@@ -37,7 +37,6 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         let recordingName = "recordedVoice.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = URL(string: pathArray.joined(separator: "/"))
-        print(filePath)
         
         let session = AVAudioSession.sharedInstance()
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
